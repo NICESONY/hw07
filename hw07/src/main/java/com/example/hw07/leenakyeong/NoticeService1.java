328f6d41f0e3a90ce7737ab488985ee3d3e03243
nanakyeong <129843360+nanakyeong@users.noreply.github.com>
2024-07-10 17:28:38 +0900

    Notice 거의 완성

--- hw07/src/main/java/com/example/hw07/leenakyeong/NoticeService1.java
+++ hw07/src/main/java/com/example/hw07/leenakyeong/NoticeService1.java
@@ -1,7 +1,37 @@
 package com.example.hw07.leenakyeong;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
-public interface NoticeService1 extends JpaRepository<Notice1, Integer> {
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class NoticeService1 {
+
+	@Autowired
+	private NoticeRepository1 noticeRepository;
+	
+	public void creat(Notice1 notice) {
+		notice.setDate(LocalDateTime.now());
+		noticeRepository.save(notice);
+	}
+	
+	public List<Notice1> readlist(){
+		return noticeRepository.findAll();
+	}
+	
+	public Notice1 readdetail(Integer id) {
+		
+		Optional <Notice1> o = noticeRepository.findById(id);
+		return o.get();
+	}
+	
+	public void delete(Integer id) {
+		noticeRepository.deleteById(id);
+	}
+	
+	public void update(Notice1 notice) {
+		noticeRepository.save(notice);
+	
+	}
 }
