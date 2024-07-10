8f747daa6007c2f9f5c99ab25c4eedb0981a4df5
nanakyeong <129843360+nanakyeong@users.noreply.github.com>
2024-07-10 17:16:02 +0900

    초안(미완성)

---
+++ hw07/src/main/java/com/example/hw07/leenakyeong/NoticeService1.java
@@ -0,0 +1,7 @@
+package com.example.hw07.leenakyeong;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeService1 extends JpaRepository<Notice1, Integer> {
+
+}
