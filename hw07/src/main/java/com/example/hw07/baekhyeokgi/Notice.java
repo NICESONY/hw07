28ef2afff403d4e066d66d830e46624dab7c2483
Gun-Hee-SON <songunhee5426@gmail.com>
2024-07-10 17:21:22 +0900

    vvvvvv

---
+++ hw07/src/main/java/com/example/hw07/baekhyeokgi/Notice.java
@@ -0,0 +1,25 @@
+package com.example.hw07.baekhyeokgi;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Notice {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;           //게시물 번호
+
+	private String title;         //게시물 제목
+	
+	private String content;       //게시물 내용
+	
+	private LocalDateTime date;   //게시물 작성일
+	
+}
